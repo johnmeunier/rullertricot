406fcda10d3d6ce39b04b0d3de86f0c8121a02ae
Johnathan MEUNIER <johnathan2693@gmail.com>
2022-07-01 14:51:38 +0200

    refacto(Tricot): optimisation des perfs en découpe en composant et génération des div

--- src/pages/Tricot/Tricot.js
+++ src/pages/Tricot/Tricot.js
@@ -1,69 +1,24 @@
 import { useRef, useState } from "react";
+import { colorFromAnImage } from "./Tricot.pure";
 import image from "../../assets/petitkoko.png";
 import "./Tricot.scss";
-
-function ColorToHex(color) {
-  var hexadecimal = color.toString(16);
-  return hexadecimal.length === 1 ? "0" + hexadecimal : hexadecimal;
-}
-
-function ConvertRGBtoHex(red, green, blue) {
-  return "#" + ColorToHex(red) + ColorToHex(green) + ColorToHex(blue);
-}
-
-const colorFromAnImage = (imagePath, width, height) => {
-  return new Promise((resolve) => {
-    const colors = [];
-    const myImg = new Image();
-    myImg.crossOrigin = "Anonymous";
-    myImg.src = imagePath;
-    myImg.onload = () => {
-      const context = document.createElement("canvas").getContext("2d");
-      context.drawImage(myImg, 0, 0);
-      let count = 0;
-      for (let i = 0; i < width; i++) {
-        count += 1;
-        colors.push([]);
-        for (let j = 0; j < height; j++) {
-          const {
-            data: [r, g, b],
-          } = context.getImageData(j, i, 1, 1);
-          colors[i].push(ConvertRGBtoHex(r, g, b));
-          count += 1;
-          console.log(`${((count * 100) / (width * height)).toFixed(2)}%`);
-        }
-      }
-      resolve(colors);
-    };
-  });
-};
 
 const App = () => {
   const [totalWidth, setTotalWidth] = useState(300);
   const [totalHeight, setTotalHeight] = useState(300);
   const [size, setSize] = useState(1);
-  const [ready, setReady] = useState("NOT READY");
+  const [colors, setColors] = useState([[]]);
 
   const imageRef = useRef();
-
-  const arrayOfMain = Array.from({ length: Math.ceil(totalWidth / size) });
 
   const imageLoaded = async () => {
     const height = imageRef.current.naturalHeight;
     const width = imageRef.current.naturalWidth;
-    setTotalHeight(imageRef.current.naturalHeight);
-    setTotalWidth(imageRef.current.naturalWidth);
+    setTotalHeight(height);
+    setTotalWidth(width);
 
-    const colors = await colorFromAnImage(image, height, width);
-    for (let i = 0; i < height - 1; i++) {
-      for (let j = 0; j < width - 1; j++) {
-        console.log(i, j);
-        console.log(document.querySelector(`div[data-x="${i}"][data-y="${j}"]`));
-        document.querySelector(`div[data-x="${i}"][data-y="${j}"]`).style.backgroundColor = colors[i][j];
-      }
-    }
-    console.log("fini");
-    setReady("READY !!!");
+    const colorsGenerated = await colorFromAnImage(image, height, width);
+    setColors(colorsGenerated);
   };
 
   return (
@@ -76,19 +31,18 @@ const App = () => {
         Maille W : {totalWidth / size}
         Maille H : {totalHeight / size}
       </header>
-      <h3>{ready}</h3>
       <div className="tool">
         <img src={image} onLoad={imageLoaded} ref={imageRef} alt="mainImage" />
-        <div className="border" style={{ width: `${totalWidth}px`, height: `${totalHeight}px` }}>
-          {arrayOfMain.map((_, i) => (
+        <div className="border" style={{ width: `${totalWidth * size}px`, height: `${totalHeight * size}px` }}>
+          {colors.map((line, i) => (
             <div className="line" key={`line-${i}`}>
-              {arrayOfMain.map((_, j) => (
+              {line.map((c, j) => (
                 <div
                   className="square"
                   data-x={i}
                   data-y={j}
                   key={`square-${i}-${j}`}
-                  style={{ minWidth: size, minHeight: size, maxWidth: size, maxHeight: size }}
+                  style={{ minWidth: size, minHeight: size, maxWidth: size, maxHeight: size, backgroundColor: c }}
                 ></div>
               ))}
             </div>
---
+++ src/pages/Tricot/Tricot.pure.js
@@ -0,0 +1,33 @@
+const colorToHex = (color) => {
+  var hexadecimal = color.toString(16);
+  return hexadecimal.length === 1 ? "0" + hexadecimal : hexadecimal;
+};
+
+const convertRGBtoHex = (red, green, blue) => `#${colorToHex(red)}${colorToHex(green)}${colorToHex(blue)}`;
+
+export const colorFromAnImage = (imagePath, width, height) => {
+  return new Promise((resolve) => {
+    const colors = [];
+    const myImg = new Image();
+    myImg.crossOrigin = "Anonymous";
+    myImg.src = imagePath;
+    myImg.onload = () => {
+      const context = document.createElement("canvas").getContext("2d");
+      context.drawImage(myImg, 0, 0);
+      let count = 0;
+      for (let i = 0; i < width; i++) {
+        count += 1;
+        colors.push([]);
+        for (let j = 0; j < height; j++) {
+          const {
+            data: [r, g, b],
+          } = context.getImageData(j, i, 1, 1);
+          colors[i].push(convertRGBtoHex(r, g, b));
+          count += 1;
+          console.log(`${((count * 100) / (width * height)).toFixed(2)}%`);
+        }
+      }
+      resolve(colors);
+    };
+  });
+};
